ae2d397479a3464bffa5e212d62f5963a818e62c
Ryan <49297331+USAFrenzy@users.noreply.github.com>
2023-03-25 19:43:19 -0400

    Progress up to module 3.2 -> should review vector math as it took me a little longer than I'd like to admit to understand why length was calculated the way it was...

--- Raytracer_Sandbox/sandbox.cpp
+++ Raytracer_Sandbox/sandbox.cpp
@@ -1,11 +1,39 @@
 
 #include <RMRT/Raytracer.h>
 
-#include <stdio.h>
+#include <iostream>
 
 int main()
 {
 
-	fprintf(stdout, rmrt::TestFunc());
+	// Image
+	constexpr int img_width{ 256 };
+	constexpr int img_height{ 256 };
 
+	// Render
+	std::cout << "P3\n" << img_width << " " << img_height << "\n255\n";
+
+	// Extracting the variables from within the loop that the original author  had being initialized in the loop each iteration
+	constexpr auto b{ 0.25 };
+	constexpr auto rgb_factor{ 255.999 };
+	constexpr auto cWidth{ img_width - 1 };
+	constexpr auto cHeight{ img_height - 1 };
+
+	for (auto i{ cHeight }; i >= 0; --i) {
+		// Adding a progress indicator
+		std::cerr << "\rScanlines Remaining: " << i << ' ' << std::flush;
+
+		for (auto j{ 0 }; j < img_width; ++j) {
+			auto r{ static_cast<double>(j) / cWidth };
+			auto g{ static_cast<double>(i) / cHeight };
+
+			auto ir{ static_cast<int>(rgb_factor * r) };
+			auto ig{ static_cast<int>(rgb_factor * g) };
+			auto ib{ static_cast<int>(rgb_factor * b) };
+
+			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+		}
+	}
+	// Progress completed
+	std::cerr << "\nDone.\n";
 }
--- include/RMRT/Raytracer.h
+++ include/RMRT/Raytracer.h
@@ -3,6 +3,27 @@
 
 namespace rmrt {
 
-const char* TestFunc();
+	class vec3 {
+	public:
+		vec3() :e{ 0,0,0 } {}
+		vec3(double e0, double e1, double e2) :e{ e0, e1, e2 } {}
+		double x() const;
+		double y() const;
+		double z() const;
+		double length() const;
+		double length_squared() const;
 
+		// operator overloads
+		vec3 operator-() const;
+		double operator[](int i) const;
+		double& operator[](int i);
+		vec3& operator +=(const vec3& v);
+		vec3& operator*=(const double t);
+		vec3& operator/=(const double t);
+
+	public:
+		double e[3];
+	};
+	using point3 = vec3;
+	using color = vec3;
 }
