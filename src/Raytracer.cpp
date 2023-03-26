4fa8807025d1d3df1b4ce8d59fdcbfbd55eafab9
Ryan <49297331+USAFrenzy@users.noreply.github.com>
2023-03-25 23:09:43 -0400

    added note for myself about vector length as it's been a hot minute since doing anything vector related

--- src/Raytracer.cpp
+++ src/Raytracer.cpp
@@ -20,6 +20,31 @@ namespace rmrt {
 		return e[2];
 	}
 
+	// Pythagorean's theorem for vec length
+	/*********************************************************************************************************************************
+	*  Given that it's been a decade since I've done any vector math, the methodology goes as follows:
+	* 
+	*  1) We have a vector at some point (x, y, z)
+	*                                        z \         | y
+	*                                            \      |
+	*                                            \    |
+	*                                             \ |
+	*                                             ----------------------- x
+	*   2) To find the final length value given vector points, we plot the points on the graph
+	*   3) We have the height of the vector triangle as our 'z' axis and the width of our vector triangle as our 'x' or 'y' axis
+	*   4) To solve for the hypotenuse, our vector legnth, we need to first find the base value of our vector triangle
+	*   5) To solve for the base value, we need to create an intermediary right triangle using the other points of our vector
+	*   6) We then solve for the intermediatary right triangle formed between the graphs at origin ---> x  ---> y
+	*   7) Once we found the hypotenuse value of the intermediatary right triangle, that becomes the base of the vector triangle 
+	*   8) The formula then becomes:
+	*                           __________________________________
+	*     Length =   \ (    _____________  )^2
+	*                        v\ (v\ x^2 + y2   )        +  (z^2)   , where the first half with the squared square-root  cancels out and we're left with : 
+	 *   
+	 *                          ____________________________
+	 *   Length = v\ (x^2) + (y^2 )+ (z^2)
+	* 
+	*********************************************************************************************************************************/
 	double vec3::length() const
 	{
 		return std::sqrt(length_squared());
